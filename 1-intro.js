de62101cea5ec070b6bae69c686ea2692101b71b
tianxu <xueqingxu0302@gmail.com>
2022-07-29 17:59:06 -0600

    module

---
+++ 1-intro.js
@@ -0,0 +1,9 @@
+const amount = 9
+
+if(amount < 10){
+    console.log('small num')
+}else{
+    console.log('larger num')
+}
+
+console.log(`hey it's my 1st code.`)
---
+++ 2-global.js
@@ -0,0 +1,19 @@
+// globals    - no window!!
+// __driname  - path to current directory
+console.log(__dirname)
+
+setInterval(()=>{
+    console.log('hello world')
+}, 1000)
+// __filename - file name
+
+// require    - function to use modules(CommonJs)
+
+// module     - info about current module (file)
+// CommonJs, every file is amodule (by default)
+// Modules - Encapsulated Code (only share minimum)
+
+// process    - info about env where the program is being executed
+
+
+
