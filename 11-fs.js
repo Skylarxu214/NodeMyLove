b7fa940ecfcfd8829e1f03aa41d42e1ff6a2422b
tianxu <xueqingxu0302@gmail.com>
2022-07-31 17:00:46 -0600

    fs2

--- 11-fs.js
+++ 11-fs.js
@@ -1,17 +1,20 @@
 const {readFileSync, writeFileSync, readFile, writeFile} = require('fs')
-
+console.log('start')
 readFile('./content/first.txt', 'utf8', (err,result) => {
+  
     if(err){
         console.log(err)
         return 
     }
     const first  = result
+    console.log('2')
     readFile('./content/second.txt', 'utf8', (err, result) =>{
         if(err){
             console.log(err)
             return 
         }
         const second  = result
+        console.log('3')
         writeFile('content/result-async.txt',
         `Here is the result: ${first}, ${second}`, (err, result) => {
             if(err){
@@ -19,6 +22,8 @@ readFile('./content/first.txt', 'utf8', (err,result) => {
                 return 
             }
             console.log(result)
+            console.log('4')
         })
     })
 })
+console.log('1')
---
+++ 9-path-module.js
@@ -0,0 +1,16 @@
+const path = require('path')
+
+console.log(path.sep)
+
+const filePath = path.join('./content', 'subfolder','text.txt')
+
+console.log(filePath)
+//=> content/subfolder/text.txt
+
+const base = path.basename(filePath)
+console.log(base)
+//=>text.txt
+
+const absolute = path.resolve(__dirname,'content','subfolder','test.txt')
+console.log(absolute)
+// =>/Users/tianx/Documents/NodeJs/content/subfolder/test.txt
